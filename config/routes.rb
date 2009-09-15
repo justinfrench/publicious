ff28594b52052252a2afc7bc10b70c9769ce1180
Justin French <justin@indent.com.au>
2009-09-16 06:44:49 +1000

    append to prev

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.stylesheet '/stylesheets/*path', :controller => "plugin_assets", :action => "show"
-  map.image      '/images/*path',      :controller => "plugin_assets", :action => "show"
-  map.javascript '/javascripts/*path', :controller => "plugin_assets", :action => "show"
+  map.stylesheet '/stylesheets/*path', :controller => "publicious", :action => "show"
+  map.image      '/images/*path',      :controller => "publicious", :action => "show"
+  map.javascript '/javascripts/*path', :controller => "publicious", :action => "show"
 
 end
--- tasks/publicious.rake
+++ tasks/publicious.rake
@@ -1,4 +1,4 @@
 # desc "Explaining what the task does"
-# task :plugin_assets do
+# task :publicious do
 #   # Task goes here
 # end
